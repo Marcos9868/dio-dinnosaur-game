4cf4f717e7504d91c2dd5edc36c87f4089043ea3
Marcos9868 <marcos_m_ferreira@outlook.com>
2021-10-21 13:53:01 -0300

    Add down movement on Dino

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,16 @@ function Jump() {
     let upInterval = setInterval(() => {
     if (position >= 150) {
         clearInterval(upInterval)
+        let downInterval = setInterval(() => {
+            if (position <= 0) {
+                clearInterval(downInterval)
+            } else {
+                position -= 20 // Demora 20 milissegundos para descer
+                dino.style.bottom = position + 'px'
+            }    
+        }, 20)
     } else {
-        position += 20  // Cada 20 milissegundos ele muda a posição
+        position += 20  // Demora 20 milissegundos para subir
         dino.style.bottom = position + 'px'
     }
     
